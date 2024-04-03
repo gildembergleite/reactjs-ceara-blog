d375d207cf25051acf0c9ef6ee309d916de80152
Gildemberg Leite <gildembergleite@gmail.com>
2024-04-02 21:52:04 -0300

    feat: create highlight home

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,3 +1,9 @@
-export default function Home() {
-  return <h1>HOME</h1>
+import { HighlightHome } from '@/components/highlight-home'
+
+export default function HomePage() {
+  return (
+    <>
+      <HighlightHome />
+    </>
+  )
 }
---
+++ src/components/highlight-home.tsx
@@ -0,0 +1,59 @@
+import { AvatarWithFallback } from '@/components/avatar-with-fallback'
+import { Badge } from '@/components/ui/badge'
+import { Card, CardContent, CardFooter, CardHeader } from '@/components/ui/card'
+import { formatDate } from '@/utils/format-date'
+import Image from 'next/image'
+import Link from 'next/link'
+
+export function HighlightHome() {
+  return (
+    <section className="relative w-full max-w-7xl h-full aspect-video">
+      <Image
+        className="w-full h-full object-cover rounded-lg"
+        src={
+          'https://images.unsplash.com/photo-1633356122544-f134324a6cee?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
+        }
+        width={0}
+        height={0}
+        sizes="100vw"
+        alt=""
+      />
+      <Card className="absolute -bottom-10 left-14 w-full max-w-2xl bg-white">
+        <CardHeader>
+          <div className="flex gap-3 items-center">
+            <Link href={'/tags/react'}>
+              <Badge>React</Badge>
+            </Link>
+            <Link href={'/tags/tailwind'}>
+              <Badge>Tailwind CSS</Badge>
+            </Link>
+
+            <Link href={'/tags/responsive'}>
+              <Badge>Responsive</Badge>
+            </Link>
+          </div>
+        </CardHeader>
+        <CardContent>
+          <Link href={'/'}>
+            <h2 className="text-foreground font-semibold text-4xl">
+              Como desenvolver tabelas responsivas para desktop e mobile,
+              utilizando Tailwind CSS
+            </h2>
+          </Link>
+        </CardContent>
+        <CardFooter>
+          <div className="flex items-center gap-2 text-muted-foreground text-sm">
+            <Link href={'#'} className="flex items-center gap-2">
+              <AvatarWithFallback
+                image="https://github.com/gildembergleite.png"
+                name="Gildemberg Leite"
+              />
+              Gildemberg Leite
+            </Link>{' '}
+            -<span>{formatDate('2024-01-25')}</span>
+          </div>
+        </CardFooter>
+      </Card>
+    </section>
+  )
+}
