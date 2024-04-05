8a41bfa2520773407db4c4632f8e44fb3a816d98
Gildemberg Leite <gildembergleite@gmail.com>
2024-04-05 18:07:57 -0300

    refactor: create vertical post card variant

--- src/components/post-card.tsx
+++ src/components/post-card.tsx
@@ -6,32 +6,57 @@ import { BadgeTagArray } from './badge-tag-array'
 import { ImageCover } from './image-cover'
 import { Card, CardContent, CardFooter, CardHeader } from './ui/card'
 
-export function PostCard(post: Post) {
+interface PostCardProps {
+  post: Post
+  orientation?: 'vertical' | 'horizontal'
+  withoutDate?: boolean
+}
+
+export function PostCard({
+  post,
+  orientation = 'horizontal',
+  withoutDate = false,
+}: PostCardProps) {
   return (
-    <Card>
-      <CardHeader className="gap-4">
+    <Card className={`${orientation === 'vertical' && 'flex max-h-72'}`}>
+      <CardHeader
+        className={`gap-4
+        ${orientation === 'vertical' ? 'aspect-square' : 'aspect-video'}`}
+      >
         <ImageCover url={post.image} />
-        <BadgeTagArray tagArray={post.tags} style="secondary" />
+        {orientation === 'horizontal' && (
+          <BadgeTagArray tagArray={post.tags} style="secondary" />
+        )}
       </CardHeader>
-      <CardContent>
-        <Link href={`/${post.slug}`}>
-          <h2 className="text-foreground font-semibold text-2xl">
-            {post.title}
-          </h2>
-        </Link>
-      </CardContent>
-      <CardFooter>
-        <div className="flex items-center gap-2 text-muted-foreground text-sm">
-          <Link href={'#'} className="flex items-center gap-2">
-            <AvatarWithFallback
-              image={post.author.imageUrl}
-              name={post.author.name}
-            />
-            {post.author.name}
-          </Link>{' '}
-          -<span>{formatDate(post.date)}</span>
-        </div>
-      </CardFooter>
+      <div
+        className={`${
+          orientation === 'vertical' &&
+          'flex flex-col w-full justify-between pt-6 pb-2'
+        }`}
+      >
+        <CardContent className={`${orientation === 'vertical' && 'space-y-4'}`}>
+          <Link href={`/${post.slug}`}>
+            <h2 className="text-foreground font-semibold text-2xl">
+              {post.title}
+            </h2>
+          </Link>
+          {orientation === 'vertical' && (
+            <BadgeTagArray tagArray={post.tags} style="secondary" />
+          )}
+        </CardContent>
+        <CardFooter>
+          <div className="flex items-center gap-2 text-muted-foreground text-sm">
+            <Link href={'#'} className="flex items-center gap-2">
+              <AvatarWithFallback
+                image={post.author.imageUrl}
+                name={post.author.name}
+              />
+              {post.author.name}
+            </Link>
+            {!withoutDate && <span> - {formatDate(post.date)}</span>}
+          </div>
+        </CardFooter>
+      </div>
     </Card>
   )
 }
