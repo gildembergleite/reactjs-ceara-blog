315b184f10c40d816b3cb23de379a736689381bf
Gildemberg Leite <gildembergleite@gmail.com>
2024-04-17 19:09:25 -0300

    refactor: load typography in highlight home

--- src/components/highlight-home.tsx
+++ src/components/highlight-home.tsx
@@ -5,22 +5,23 @@ import { formatDate } from '@/utils/format-date'
 import Link from 'next/link'
 import { BadgeTagArray } from './badge-tag-array'
 import { ImageCover } from './image-cover'
+import { Typography } from './ui/typography'
 
 export function HighlightHome(post: Post) {
   return (
     <section className="relative w-full max-w-7xl h-full aspect-video">
       <ImageCover url={post.image} />
-      <Card className="absolute -bottom-10 left-14 w-full max-w-2xl drop-shadow-xl0">
+      <Card className="absolute -bottom-10 left-14 w-full max-w-xl drop-shadow-xl">
         <CardHeader>
-          <BadgeTagArray tagArray={post.tags} />
+          <BadgeTagArray tagArray={post.tags} style="secondary" />
         </CardHeader>
         <CardContent>
           <Link href={`/${post.slug}`}>
-            <h2 className="font-semibold text-4xl">{post.title}</h2>
+            <Typography.H2 className="border-none">{post.title}</Typography.H2>
           </Link>
         </CardContent>
         <CardFooter>
-          <div className="flex items-center gap-2 text-muted-foreground text-sm">
+          <div className="flex items-center gap-2">
             <Link
               href={`/author/${post.author.slug}`}
               className="flex items-center gap-2"
@@ -29,9 +30,9 @@ export function HighlightHome(post: Post) {
                 image={post.author.avatarUrl}
                 name={post.author.name}
               />
-              {post.author.name}
-            </Link>{' '}
-            -<span>{formatDate(post.date)}</span>
+              <Typography.Muted>{post.author.name}</Typography.Muted>
+            </Link>
+            <Typography.Muted>- {formatDate(post.date)}</Typography.Muted>
           </div>
         </CardFooter>
       </Card>
