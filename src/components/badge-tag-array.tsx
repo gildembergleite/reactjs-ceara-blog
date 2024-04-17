b096e4ccee20817fe724f393399bce86851051b2
Gildemberg Leite <gildembergleite@gmail.com>
2024-04-17 19:06:53 -0300

    style: add nowrap in whitespaces

--- src/components/badge-tag-array.tsx
+++ src/components/badge-tag-array.tsx
@@ -14,12 +14,11 @@ export function BadgeTagArray({
   size = 'sm',
 }: BadgeTagArrayProps) {
   return (
-    <>
-      <div className="flex gap-3 items-center">
-        {tagArray.map((tag, index) => (
-          <Link key={index} href={`/tags/${tag.slug}`}>
-            <Badge
-              className={`
+    <div className="flex flex-wrap w-full max-w-full gap-3 items-center">
+      {tagArray.map((tag, index) => (
+        <Link key={index} href={`/tags/${tag.slug}`}>
+          <Badge
+            className={`whitespace-nowrap
                 ${
                   style === 'secondary'
                     ? 'text-primary bg-primary/10 hover:bg-primary hover:text-white'
@@ -27,12 +26,11 @@ export function BadgeTagArray({
                 }
                 ${size === 'md' ? 'text-base' : ''}
               `}
-            >
-              {tag.name}
-            </Badge>
-          </Link>
-        ))}
-      </div>
-    </>
+          >
+            {tag.name}
+          </Badge>
+        </Link>
+      ))}
+    </div>
   )
 }
