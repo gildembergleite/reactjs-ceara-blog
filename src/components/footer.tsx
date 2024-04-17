c18c872ea339ebec4cb08f2bc63acdcace284739
Gildemberg Leite <gildembergleite@gmail.com>
2024-04-17 19:08:57 -0300

    style: add margin in footer

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -4,7 +4,7 @@ import { BlogLogo } from './blog-logo'
 export function Footer() {
   return (
     <footer className="flex justify-center items-center w-full h-24 border-t bg-background z-50">
-      <div className="flex w-full max-w-7xl justify-between items-center">
+      <div className="flex w-full max-w-7xl justify-between items-center mx-8">
         <BlogLogo />
         <div className="flex items-center gap-6">
           <Link href={''}></Link>
